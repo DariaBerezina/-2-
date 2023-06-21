faf7f8a8f437feeb904e2a22e52fee564288329b
DariaBerezina <124424280+DariaBerezina@users.noreply.github.com>
2023-06-21 08:43:29 +0600

    Add files via upload

---
+++ HOME_LAB/dto/ResultDto.hpp
@@ -0,0 +1,20 @@
+#include "oatpp/core/macro/codegen.hpp"
+#include "oatpp/core/Types.hpp"
+#include OATPP_CODEGEN_BEGIN(DTO) 
+
+class ArgumentsDto : public oatpp::DTO {
+	DTO_INIT(ArgumentsDto, DTO);
+	DTO_FIELD(Float32, a);
+	DTO_FIELD(Float32, b);
+};
+
+// опишем данные которые будем возвращать после вычисления суммы
+class ResultDto : public oatpp::DTO {
+	DTO_INIT(ResultDto, DTO);
+	DTO_FIELD(Float32, value);
+	DTO_FIELD(String, message);
+	DTO_FIELD(Object<ArgumentsDto>, arguments);
+
+};
+
+#include OATPP_CODEGEN_END(DTO)
